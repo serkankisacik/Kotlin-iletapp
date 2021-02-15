0b211033b39822153421d2d90783e3e2aa8714f3
serkankisacik <serkankisacik01@gmail.com>
2021-02-16 00:36:38 +0300

    Profil aktivitesi eklendi

---
+++ app/src/main/java/app/ilet/ProfilActivity.kt
@@ -0,0 +1,11 @@
+package app.ilet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
