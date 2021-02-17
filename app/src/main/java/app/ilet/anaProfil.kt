98898ff77a521f37e7484f7d19893ebb495d8055
serkankisacik <serkankisacik01@gmail.com>
2021-02-18 00:49:11 +0300

    Profil ve Ayarlar Fragmentleri eklendi

---
+++ app/src/main/java/app/ilet/anaProfil.kt
@@ -0,0 +1,19 @@
+package app.ilet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class anaProfil : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ana_profil, container, false)
+    }
+}
