a387078739e9eb4bcea818ef5a99d999c69557af
serkankisacik <serkankisacik01@gmail.com>
2021-04-08 14:41:52 +0300

    Seyahata gorsel ekleme denemesi Btk

--- app/src/main/java/app/ilet/ilanDurakEkle.kt
+++ app/src/main/java/app/ilet/ilanDurakEkle.kt
@@ -1,18 +1,32 @@
 package app.ilet
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_ilan_durak_ekle.*
 import kotlinx.android.synthetic.main.fragment_ilanlar.*
 
 class ilanDurakEkle : Fragment() {
+    var secilenGorsel : Uri? = null
+    var secilenBitmap : Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appContext = requireContext().applicationContext
     }
 
     override fun onCreateView(
@@ -29,5 +43,37 @@ class ilanDurakEkle : Fragment() {
             val action = ilanDurakEkleDirections.actionİlanDurakEkleToİlanOnay()
             Navigation.findNavController(it).navigate(action)
         }
+
+        imageSeyahatGorsel.setOnClickListener {
+
+            if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                // izin alınmamış
+                ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode==1){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //izin verilince
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun gorselSec(view: View){
+            if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                // izin alınmamış
+                ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
     }
 }
